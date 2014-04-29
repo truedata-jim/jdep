9b20e827e606804185812324dfc64f179e19555e
Jim Lloyd <jim@truedata.net>
2014-04-28 18:11:41 -0700

    More refactoring. Most functions now private members, and other cosmetic changes.

--- jdep.cpp
+++ jdep.cpp
@@ -69,7 +69,7 @@ struct cp_info {
     cp_info(int _tag) : tag(_tag) {}
 };
 
-cp_info* const kLongTag = ((cp_info *) -1);
+cp_info* const kLongTag = ((cp_info*) -1);
 
 struct constant_class_info : public cp_info {
     uint16_t name_index;
@@ -78,16 +78,24 @@ struct constant_class_info : public cp_info {
 };
 
 struct constant_utf8_info  : public cp_info {
-    char *str;
+    char* str;
 
     constant_utf8_info(char* _str) : cp_info(CONSTANT_Utf8), str(_str) {}
 };
 
 struct attribute_info {
-    attribute_info *next;
     uint16_t attribute_name_index;
     long attribute_length;
-    uint8_t *info;
+    uint8_t* info;
+    attribute_info* next;
+
+    attribute_info(uint16_t _attribute_name_index, long _attribute_length
+                 , uint8_t* _info, attribute_info* _next)
+    : attribute_name_index(_attribute_name_index)
+    , attribute_length(_attribute_length)
+    , info(_info)
+    , next(_next)
+    {}
 };
 
 class ClassFileAnalyzer;
@@ -102,7 +110,7 @@ public:
         cp_info* cp = mConstantPool[index];
         cp = mConstantPool[index];
         if (cp->tag == CONSTANT_Utf8) {
-            const char* name = ((constant_utf8_info *) cp)->str;
+            const char* name = ((constant_utf8_info*) cp)->str;
             return name;
         }
         return NULL;
@@ -110,17 +118,17 @@ public:
 
     const char* getClassName(int index) const
     {
-        cp_info *cp = mConstantPool[index];
+        cp_info* cp = mConstantPool[index];
         if (cp == NULL) {
             return NULL;
         } else if (cp->tag == CONSTANT_Class) {
-            constant_class_info *classInfo = (constant_class_info *) cp;
+            constant_class_info* classInfo = (constant_class_info*) cp;
             return getString(classInfo->name_index);
         } else if (cp->tag == CONSTANT_Utf8) {
-            char *name = ((constant_utf8_info *) cp)->str;
+            char* name = ((constant_utf8_info*) cp)->str;
             if (name[0] == 'L') {
-                char *result = strdup(name + 1);
-                char *s = result;
+                char* result = strdup(name + 1);
+                char* s = result;
                 while (*s) {
                     if (*s == ';') {
                         *s = '\0';
@@ -141,8 +149,16 @@ public:
 
 private:
 
-    cp_info** readConstantPool(FileReader& reader, const char *filename, int count);
+    cp_info** readConstantPool(FileReader& reader, const char* filename, int count);
+    cp_info*  readConstantPoolInfo(FileReader& reader, const char* filename);
+    attribute_info* readFields(FileReader& reader, int count, attribute_info* atts);
+    attribute_info* readFieldInfo(FileReader& reader, attribute_info* atts);
+    attribute_info* readMethods(FileReader& reader, int count, attribute_info* atts);
+    attribute_info* readMethodInfo(FileReader& reader, attribute_info* atts);
+    attribute_info* readAttributes(FileReader& reader, int count, attribute_info* atts);
+    attribute_info* readAttributeInfo(FileReader& reader, attribute_info* atts);
 
+    void skipWordArray(FileReader& reader, int length);
 
 private:
     uint16_t mConstantPoolCount;
@@ -150,32 +166,27 @@ private:
     attribute_info* mAttributes;
 };
 
-FILE *fopenPath(char *path);
-bool mkdirPath(char *path);
-cp_info **readConstantPool(FileReader& reader, const char *filename, int count);
-cp_info *readConstantPoolInfo(FileReader& reader, const char *filename);
-attribute_info *readFields(FileReader& reader, int count, attribute_info *atts);
-attribute_info *readMethods(FileReader& reader, int count, attribute_info *atts);
-void skipWordArray(FileReader& reader, int length);
+FILE* fopenPath(char* path);
+bool mkdirPath(char* path);
 
 class ClassFileAnalyzer
 {
 public:
     ClassFileAnalyzer() {}
 
-    void analyzeClassFile(char *name);
+    void analyzeClassFile(char* name);
 
     void includePackage(const string& name) { mIncludedPackages.insert(PackageToPath(name)); }
 
     void excludePackage(const string& name) { mExcludedPackages.insert(PackageToPath(name)); }
 
-    bool addDep(const char *name);
+    bool addDep(const char* name);
         // Adds name to the set of known dependencies.
         // Returns true if this is a new dependency.
 
     bool isIncludedClass(const string& name) const;
 
-    void findDeps(const char *name);
+    void findDeps(const char* name);
 
     void SetJavaRoot(const string& root) { mJavaRoot = SavePath(root); }
     void SetClassRoot(const string& root) { mClassRoot = SavePath(root); }
@@ -207,20 +218,20 @@ private:
     string mDepRoot;
 };
 
-bool ClassFileAnalyzer::addDep(const char *name)
+bool ClassFileAnalyzer::addDep(const char* name)
 {
     int before = mDeps.size();
     mDeps.insert(name);
     return before != mDeps.size();
 }
 
-void ClassFileAnalyzer::analyzeClassFile(char *name)
+void ClassFileAnalyzer::analyzeClassFile(char* name)
 {
-    FILE *outfyle;
+    FILE* outfyle;
     char outfilename[1000];
     int i;
 
-    char *match = strstr(name, ".class");
+    char* match = strstr(name, ".class");
     if (match && strlen(match) == 6 /* strlen(".class") */) {
         /* Chop off the trailing ".class" if it's there */
         *match = '\0';
@@ -257,17 +268,6 @@ void ClassFileAnalyzer::analyzeClassFile(char *name)
     }
 }
 
-attribute_info * build_attribute_info(uint16_t attribute_name_index, long attribute_length,
-                     uint8_t *info, attribute_info *next)
-{
-    attribute_info *result = TYPE_ALLOC(attribute_info);
-    result->next = next;
-    result->attribute_name_index = attribute_name_index;
-    result->attribute_length = attribute_length;
-    result->info = info;
-    return result;
-}
-
 string ClassFileAnalyzer::PackageToPath(const string& name)
 {
     string pathName(name);
@@ -277,9 +277,9 @@ string ClassFileAnalyzer::PackageToPath(const string& name)
     return pathName;
 }
 
-void ClassFileAnalyzer::findDeps(const char *name)
+void ClassFileAnalyzer::findDeps(const char* name)
 {
-    FILE *infyle;
+    FILE* infyle;
     char infilename[1000];
 
     snprintf(infilename, sizeof(infilename), "%s%s.class", mClassRoot.c_str(), name);
@@ -314,7 +314,7 @@ void ClassFile::scanAnnotation(BytesDecoder& decoder, ClassFileAnalyzer& analyze
 
     int type_index = decoder.DecodeWord();
 
-    const char *name = getClassName(type_index);
+    const char* name = getClassName(type_index);
     if (analyzer.isIncludedClass(name)) {
         analyzer.addDep(name);
     }
@@ -350,7 +350,7 @@ void ClassFile::scanElementValue(BytesDecoder& decoder, ClassFileAnalyzer& analy
         }
         case 'e': {
             int type_name_index = decoder.DecodeWord();
-            const char *name = getClassName(type_name_index);
+            const char* name = getClassName(type_name_index);
             int const_name_index = decoder.DecodeWord();
             if (analyzer.isIncludedClass(name)) {
                 analyzer.addDep(name);
@@ -382,7 +382,7 @@ void ClassFile::findDepsInFile(const char* target, ClassFileAnalyzer& analyzer)
             const char* name = getClassName(i);
             if (analyzer.isIncludedClass(name)) {
                 if (name[0] != '[') { /* Skip array classes */
-                    char *dollar = index(name, '$');
+                    char* dollar = index(name, '$');
                     if (dollar) {
                         /* It's an inner class */
                         if (strncmp(name, target, dollar-name) == 0) {
@@ -409,9 +409,9 @@ void ClassFile::findDepsInFile(const char* target, ClassFileAnalyzer& analyzer)
         }
     }
 
-    attribute_info *att = mAttributes;
+    attribute_info* att = mAttributes;
     while (att != NULL) {
-        const char *name = getString(att->attribute_name_index);
+        const char* name = getString(att->attribute_name_index);
         if (strcmp(name, "RuntimeVisibleAnnotations") == 0) {
             int i;
             BytesDecoder decoder(att->info, att->attribute_length);
@@ -424,9 +424,9 @@ void ClassFile::findDepsInFile(const char* target, ClassFileAnalyzer& analyzer)
     }
 }
 
-FILE * fopenPath(char *path)
+FILE * fopenPath(char* path)
 {
-    char *end = rindex(path, '/');
+    char* end = rindex(path, '/');
     if (end) {
         *end = '\0';
         mkdirPath(path);
@@ -435,10 +435,10 @@ FILE * fopenPath(char *path)
     return fopen(path, "w");
 }
 
-bool mkdirPath(char *path)
+bool mkdirPath(char* path)
 {
-    char *slashptr = path;
-    DIR *dyr;
+    char* slashptr = path;
+    DIR* dyr;
 
     while ((slashptr = strchr(slashptr + 1, '/'))) {
         *slashptr = '\0';
@@ -462,22 +462,19 @@ bool mkdirPath(char *path)
     }
 }
 
-attribute_info * readAttributeInfo(FileReader& reader, attribute_info *atts)
+attribute_info* ClassFile::readAttributeInfo(FileReader& reader, attribute_info* atts)
 {
     uint16_t attribute_name_index = reader.ReadWord();
     long attribute_length = reader.ReadLong();
-    uint8_t *info = reader.ReadByteArray(attribute_length);
+    uint8_t* info = reader.ReadByteArray(attribute_length);
 
-    return build_attribute_info(attribute_name_index, attribute_length, info,
-                                atts);
+    return new attribute_info(attribute_name_index, attribute_length, info, atts);
 }
 
-attribute_info * readAttributes(FileReader& reader, int count, attribute_info *atts)
+attribute_info* ClassFile::readAttributes(FileReader& reader, int count, attribute_info* atts)
 {
-    int i;
-    for (i = 0; i < count; ++i) {
+    for (int i=0; i<count; ++i)
         atts = readAttributeInfo(reader, atts);
-    }
     return atts;
 }
 
@@ -488,25 +485,25 @@ ClassFile::ClassFile(const char* infilename)
 {
     FileReader reader(infilename);
 
-    reader.ReadLong(); /* magic */
-    reader.ReadWord(); /* minor_version */
-    reader.ReadWord(); /* major_version */
+    reader.ReadLong(); // magic
+    reader.ReadWord(); // minor_version
+    reader.ReadWord(); // major_version
     mConstantPoolCount = reader.ReadWord();
     mConstantPool = readConstantPool(reader, infilename, mConstantPoolCount);
-    reader.ReadWord(); /* access_flags */
-    reader.ReadWord(); /* this_class */
-    reader.ReadWord(); /* super_class */
+    reader.ReadWord(); // access_flags
+    reader.ReadWord(); // this_class
+    reader.ReadWord(); // super_class
     uint16_t interfaces_count = reader.ReadWord();
-    skipWordArray(reader, interfaces_count); /* interfaces */
+    skipWordArray(reader, interfaces_count); // interfaces
     uint16_t fields_count = reader.ReadWord();
-    mAttributes = readFields(reader, fields_count, mAttributes); /* fields */
+    mAttributes = readFields(reader, fields_count, mAttributes); // fields
     uint16_t methods_count = reader.ReadWord();
-    mAttributes = readMethods(reader, methods_count, mAttributes); /* methods */
+    mAttributes = readMethods(reader, methods_count, mAttributes); // methods
     uint16_t attributes_count = reader.ReadWord();
     mAttributes = readAttributes(reader, attributes_count, mAttributes);
 }
 
-cp_info** ClassFile::readConstantPool(FileReader& reader, const char *filename, int count)
+cp_info** ClassFile::readConstantPool(FileReader& reader, const char* filename, int count)
 {
     cp_info** result = new cp_info*[count];
     int i;
@@ -521,7 +518,7 @@ cp_info** ClassFile::readConstantPool(FileReader& reader, const char *filename, 
     return result;
 }
 
-cp_info * readConstantPoolInfo(FileReader& reader, const char *filename)
+cp_info* ClassFile::readConstantPoolInfo(FileReader& reader, const char* filename)
 {
     uint8_t tag = reader.ReadByte();
     switch (tag) {
@@ -530,70 +527,72 @@ cp_info * readConstantPoolInfo(FileReader& reader, const char *filename)
             return new constant_class_info(name_index);
         }
         case CONSTANT_Fieldref:{
-            reader.ReadWord(); /* class_index */
-            reader.ReadWord(); /* name_and_type_index */
+            reader.ReadWord(); // class_index
+            reader.ReadWord(); // name_and_type_index
             return NULL;
         }
         case CONSTANT_Methodref:{
-            reader.ReadWord(); /* class_index */
-            reader.ReadWord(); /* name_and_type_index */
+            reader.ReadWord(); // class_index
+            reader.ReadWord(); // name_and_type_index
             return NULL;
         }
         case CONSTANT_InterfaceMethodref:{
-            reader.ReadWord(); /* class_index */
-            reader.ReadWord(); /* name_and_type_index */
+            reader.ReadWord(); // class_index
+            reader.ReadWord(); // name_and_type_index
             return NULL;
         }
         case CONSTANT_String:{
-            reader.ReadWord(); /* string_index */
+            reader.ReadWord(); // string_index
             return NULL;
         }
         case CONSTANT_Integer:{
-            reader.ReadLong(); /* bytes */
+            reader.ReadLong(); // bytes
             return NULL;
         }
         case CONSTANT_Float:{
-            reader.ReadLong(); /* bytes */
+            reader.ReadLong(); // bytes
             return NULL;
         }
         case CONSTANT_Long:{
-            reader.ReadLong(); /* high_bytes */
-            reader.ReadLong(); /* low_bytes */
+            reader.ReadLong(); // high_bytes
+            reader.ReadLong(); // low_bytes
             return kLongTag;
         }
         case CONSTANT_Double:{
-            reader.ReadLong(); /* high_bytes */
-            reader.ReadLong(); /* low_bytes */
+            reader.ReadLong(); // high_bytes
+            reader.ReadLong(); // low_bytes
             return kLongTag;
         }
         case CONSTANT_NameAndType:{
-            reader.ReadWord(); /* name_index */
-            reader.ReadWord(); /* descriptor_index */
+            reader.ReadWord(); // name_index
+            reader.ReadWord(); // descriptor_index
             return NULL;
         }
         case CONSTANT_Utf8:{
             uint16_t length = reader.ReadWord();
-            char *str = (char *) reader.ReadByteArray(length);
+            char* str = (char* ) reader.ReadByteArray(length);
             return new constant_utf8_info(str);
         }
         default:
+        {
             fprintf(stderr, "invalid constant pool tag %d in %s\n", tag,
                     filename);
             exit(1);
+        }
     }
     return NULL;
 }
 
-attribute_info * readFieldInfo(FileReader& reader, attribute_info *atts)
+attribute_info* ClassFile::readFieldInfo(FileReader& reader, attribute_info* atts)
 {
-    reader.ReadWord(); /* access_flags */
-    reader.ReadWord(); /* name_index */
-    reader.ReadWord(); /* descriptor_index */
+    reader.ReadWord(); // access_flags
+    reader.ReadWord(); // name_index
+    reader.ReadWord(); // descriptor_index
     uint16_t attributes_count = reader.ReadWord();
     return readAttributes(reader, attributes_count, atts);
 }
 
-attribute_info * readFields(FileReader& reader, int count, attribute_info *atts)
+attribute_info* ClassFile::readFields(FileReader& reader, int count, attribute_info* atts)
 {
     int i;
     for (i=0; i<count; ++i) {
@@ -602,36 +601,32 @@ attribute_info * readFields(FileReader& reader, int count, attribute_info *atts)
     return atts;
 }
 
-attribute_info * readMethodInfo(FileReader& reader, attribute_info *atts)
+attribute_info* ClassFile::readMethodInfo(FileReader& reader, attribute_info* atts)
 {
-    reader.ReadWord(); /* access_flags */
-    reader.ReadWord(); /* name_index */
-    reader.ReadWord(); /* descriptor_index */
+    reader.ReadWord(); // access_flags
+    reader.ReadWord(); // name_index
+    reader.ReadWord(); // descriptor_index
     uint16_t attributes_count = reader.ReadWord();
     return readAttributes(reader, attributes_count, atts);
 }
 
-attribute_info * readMethods(FileReader& reader, int count, attribute_info *atts)
+attribute_info * ClassFile::readMethods(FileReader& reader, int count, attribute_info* atts)
 {
-    int i;
-    for (i = 0; i < count; ++i) {
+    for (int i = 0; i < count; ++i)
         atts = readMethodInfo(reader, atts);
-    }
     return atts;
 }
 
-void skipWordArray(FileReader& reader, int length)
+void ClassFile::skipWordArray(FileReader& reader, int length)
 {
-    int i;
-    for (i = 0; i < length; ++i) {
+    for (int i = 0; i < length; ++i)
         reader.ReadWord();
-    }
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int i;
-    char *p;
+    char* p;
     bool excludeLibraryPackages = true;
     const char* usage = "usage: jdep -a [-e PACKAGE] [-i PACKAGE] -h [-c CPATH] [-d DPATH] [-j JPATH] files...\n";
 
